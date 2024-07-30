bda639ff62944b65ff95e1e2beb71c17c758199c
Ka-YJS <ljs971021@naver.com>
2024-07-30 17:44:52 +0900

    Add files via upload

---
+++ Print01.java
@@ -0,0 +1,28 @@
+package ex02_print;
+
+public class Print01 {
+
+	public static void main(String[] args) {
+		System.out.print("WelCome");
+		System.out.println("Java World");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
