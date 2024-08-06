af4cb9b3297f0efcfd5ed21f3e88823408aa1146
Ka-YJS <ljs971021@naver.com>
2024-08-06 22:54:01 +0900

    Update Bread.java

--- EX_0806/src/ex03_method/Bread.java
+++ EX_0806/src/ex03_method/Bread.java
@@ -4,31 +4,25 @@ import java.util.Scanner;
 
 public class Bread {
 	
-	String name;
-	Scanner sc = new Scanner(System.in);
-	int num = sc.nextInt();
-	
-	
 	public void makeBread() {
 		System.out.println("빵을 만들었습니다.");
+		System.out.println("----------------------");
 	}
 	
-
-	public void makeBread(int num) {
-		System.out.print("빵 갯수 입력 : "+num);
-		this.num = num;
-		
-		for(int i=0;i<num;i++) {
-			System.out.printf("빵을 만들었습니다.");
-		}System.out.printf("요청하신 %d개의 빵을 만들었습니다.",num);
+	public void makeBread(int n) {
+		for(int i = 0; i < n; i++) {
+			System.out.println("빵을 만들었습니다.");
+		}
+		System.out.println("요청하신 "+n+"개의 빵을 만들었습니다.");
+		System.out.println("----------------------");
 	}
 	
-	public void makeBread(int num, String name) {
-		for(int i=0;i<num;i++) {
-			System.out.printf("빵을 만들었습니다.");
-			
-		}System.out.printf("요청하신 %d개의 %s빵을 만들었습니다.",num,name);
-		
+	public void makeBread(int n, String name) {
+		for(int i = 0; i < n; i++) {
+			System.out.println(name+"빵을 만들었습니다.");
+		}
+		System.out.println("요청하신 "+n+"개의 "+name+"빵을 만들었습니다.");
+		System.out.println("----------------------");
 	}
 	
 
