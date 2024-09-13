e02d11f61791066d6535d8edc7d6709a6e8ae3b8
Ka-YJS <ljs971021@naver.com>
2024-09-13 17:12:05 +0900

    Update App.js

--- EX_0913/App.js
+++ EX_0913/App.js
@@ -12,7 +12,9 @@ function App() {
     <div className="App">
       <Sol3 />
        {/*3. : div안쪽 내용 다 지우고 <??? />작성해주기 
-              ->??? : Sol1, Sol2, Sol3*/}
+              ->??? : Sol1, Sol2, Sol3
+        + <React.StricMode> : 이 태그는 애플리케이션의 잠재적인 문제를 알아내기 위한 도구로,
+                            위에 나오는 <div>안에 넣어주고 Sol3을 실행시키면 "랜더링 완료"가 2번씩 실행됨*/}
     </div>
   );
 }
