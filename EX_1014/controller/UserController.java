318a7db02658d2274112672d8425e1e020c2ea39
Ka-YJS <ljs971021@naver.com>
2024-10-15 08:33:50 +0900

    Update UserController.java

--- EX_1014/controller/UserController.java
+++ EX_1014/controller/UserController.java
@@ -13,68 +13,87 @@ import org.springframework.web.bind.annotation.RestController;
 import com.korea.user.dto.ResponseDTO;
 import com.korea.user.dto.UserDTO;
 import com.korea.user.model.UserEntity;
-import com.korea.user.persistence.UserRepository;
 import com.korea.user.security.TokenProvider;
 import com.korea.user.service.UserService;
 
 @RestController
-@RequestMapping("users")
+@RequestMapping("/users")
 public class UserController {
-	
+
 	@Autowired
 	private UserService userService;
+	
 	@Autowired
-	private TokenProvider tokenProvider;
+	private TokenProvider tokenProvider;//토큰을 발급해줌
 	
 	//id중복조회
 	@GetMapping("idCheck")
-	public ResponseEntity<?>isIdDuplicate(@RequestBody UserDTO dto){
+	public ResponseEntity<?> isIdDuplicate(@RequestBody UserDTO dto){
 		boolean check = userService.isIdDuplicate(dto.getUserId());
-		ResponseDTO<Boolean>response = ResponseDTO.<Boolean>builder().value(check).build();
+		ResponseDTO<Boolean> response = ResponseDTO.<Boolean>builder().value(check).build();
 		return ResponseEntity.ok().body(response);
 	}
 	
 	@PostMapping("signup")
-	public ResponseEntity<?>signup(@RequestBody UserDTO dto){
+	//id
+	//pwd
+	//name
+	//email
+	public ResponseEntity<?> sginup(@RequestBody UserDTO dto){
 		List<UserDTO> dtos = userService.insert(dto);
 		ResponseDTO<UserDTO> response = ResponseDTO.<UserDTO>builder().data(dtos).build();
 		return ResponseEntity.ok().body(response);
-				
 	}
 	
 	//모든 유저 조회
 	@GetMapping("allUsers")
-	public ResponseEntity<?>allUsers(){
-		List<UserDTO>dtos = userService.allUsers();
+	public ResponseEntity<?> allUsers(){
+		List<UserDTO> dtos = userService.allUsers();
 		ResponseDTO<UserDTO> response = ResponseDTO.<UserDTO>builder().data(dtos).build();
-		return ResponseEntity.ok().body(response); 
+		return ResponseEntity.ok().body(response);
 	}
 	
 	//로그인
 	@PostMapping("/signin")
-	public ResponseEntity<?>authenticate(@RequestBody UserDTO dto){
-		//아이디와 비밀번호를 입력받음
+	//아이디와 비밀번호를 입력받는다.
+	public ResponseEntity<?> authenticate(@RequestBody UserDTO dto){
+		//id와 비밀번호를 통해 유저를 반환받는다.
 		UserEntity user = userService.getByCredential(dto.getUserId(), dto.getPwd());
 		
 		//조회가 됐다면
-		if(user !=null) {
-			//토큰을 발급해줌
+		if(user != null) {
+			//토큰을 발급해준다.
 			final String token = tokenProvider.create(user);
 			
 			final UserDTO responseUserDTO = UserDTO.builder()
-														.userId(user.getUserId())
-														.idx(user.getIdx())
-														.name(user.getName())
-														.email(user.getEmail())
-														.token(token)
-														.build();
+												.userId(user.getUserId())
+												.idx(user.getIdx())
+												.name(user.getName())
+												.email(user.getEmail())
+												.token(token)
+												.build();
 			return ResponseEntity.ok().body(responseUserDTO);
-		}else {
+		} else {
 			ResponseDTO responseDTO = ResponseDTO.builder().error("Login failed").build();
-			return ResponseEntity.ok().body(responseDTO);
+			return ResponseEntity.badRequest().body(responseDTO);
 		}
 	}
 	
 	
-
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
