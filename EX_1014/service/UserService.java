675dc6d67d0706cc54acff0fd1f7dd3af38273b0
Ka-YJS <ljs971021@naver.com>
2024-10-15 08:30:44 +0900

    Update UserService.java

--- EX_1014/service/UserService.java
+++ EX_1014/service/UserService.java
@@ -15,52 +15,58 @@ import com.korea.user.persistence.UserRepository;
 public class UserService {
 	
 	@Autowired
-	private UserRepository userRepository;
-	
-	/*1. 회원추가
-	 -id : 중복되는게 있는지 먼저 검증
-	 -pwd
-	 -name
-	 -email
-	 
-	 1_1. id중복체크 메서드 만들기 -> 메서드명 : boolean isIdDuplicated(String id)
-	 1_2. 회원추가 -> 메서드명 : List<UserDTO>insert(UserDTO dto)
-	 */
-	
-	/*id 중복체크메서드 : id를 db로 전달해서 조회하면 false, 조회안되면 true를 반환
-	 *true면 아이디 생성가능, false면 아이디생성 불가능
-	 */
+	UserRepository userRepository;
 
+	//id중복체크메서드
+	//id를 db로 전달해서 조회가 되면 false, 조회가 안되면 true를 반환
+	//true면 아이디 생성 가능, false면 아이디 생성 불가능
 	public boolean isIdDuplicate(String id) {
-		Optional<UserEntity> user =  userRepository.findByUserId(id);
-		return !user.isPresent( );//조회되면 true
+		System.out.println(id);
+		Optional<UserEntity> user = userRepository.findByUserId(id);
+		return !user.isPresent();//조회되면 true...
 	}
 	
-	//2. 회원가입메서드
+	//회원가입메서드
 	public List<UserDTO> insert(UserDTO dto){
 		
 		//dto -> entity
 		UserEntity entity = UserDTO.toEntity(dto);
 		
-		//entity를 DB에 저장
+		//entity를 db에 저장
 		userRepository.save(entity);
 		
 		//List<UserEntity> -> List<UserDTO>
 		return userRepository.findAll().stream().map(UserDTO::new).collect(Collectors.toList());
 		
+		
 	}
 	
-	//유저전체조회
-	public List<UserDTO>allUsers(){
-		List<UserDTO> list = userRepository.findAll().stream().map(UserDTO::new).collect(Collectors.toList());
+	//유저 전체조회
+	public List<UserDTO> allUsers(){
+		List<UserDTO> list = userRepository.findAll().stream()
+								.map(UserDTO::new)
+								.collect(Collectors.toList());
 		return list;
 	}
 	
 	
-	//로그인 -> 아이디랑 비밀번호를 받음
+	//로그인
+	//아이디랑 비밀번호를 받는다.
 	public UserEntity getByCredential(String userId, String pwd) {
-		return userRepository.findByUserIdAndPwd(userId, pwd);
+		return userRepository.findByuserIdAndPwd(userId,pwd);
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
