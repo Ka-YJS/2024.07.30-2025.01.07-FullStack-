5f04c90718a8adaa65343396bacface20f16d3f3
Ka-YJS <ljs971021@naver.com>
2024-11-04 17:48:57 +0900

    Update Length.js

--- RCT_NATIVE/EX_1104/hooks/src/components_hk/Length.js
+++ RCT_NATIVE/EX_1104/hooks/src/components_hk/Length.js
@@ -19,13 +19,15 @@ let idx = 0;
 
 const Length = () => {
     const [text, setText] = useState(list[0]);
-    const [length, setLength] = useState('');
+    // const [length, setLength] = useState('');
 
     const _onPress = () => {
-        setLength(getLength(text));
+        // setLength(getLength(text));
         ++idx;
         if(idx<list.length) setText(list[idx]);
     }
+
+    const length = useMemo(() => getLength(text),[text])
 
     return(
         <>
