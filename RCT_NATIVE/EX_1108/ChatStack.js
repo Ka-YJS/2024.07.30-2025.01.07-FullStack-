82caebf1a2cba75c27cea2e1cf5c34f696db795a
Ka-YJS <ljs971021@naver.com>
2024-11-08 17:36:47 +0900

    Add files via upload

---
+++ RCT_NATIVE/EX_1108/ChatStack.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { ChatListScreen, ChatDetailScreen } from "../screens/ChatScreens";
+
+const ChatStack = createStackNavigator();
+
+const ChatStackNavigator = () => {
+
+    return(
+
+    <ChatStack.Navigator>
+        <ChatStack.Screen name="ChatList" component={ChatListScreen}/>
+        <ChatStack.Screen name="ChatDetail" component={ChatDetailScreen}
+            options={{
+                tabBarStyle : {display:'none'}
+            }}/>
+    </ChatStack.Navigator>
+
+    )
+}
+
+export default ChatStackNavigator;
